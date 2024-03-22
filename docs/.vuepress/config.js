c6b93d8aad961d1b76ecd3ecaf3a5897b0544e5f
NULL <1014218621@qq.com>
2024-03-23 00:12:39 +0800

    feature: 构建新目录，添加gitignore

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,41 @@
 module.exports = {
   title: "NULL博客",
-  description: "分享各类资源、教程、黑科技软件、工具等等",
+  description: "分享各类资源、教程、工具等等",
   markdown: {
     lineNumbers: true,
   },
   themeConfig: {
-    sidebar: [
+    lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
+    nav:[
+      { text: 'NULL主页', link: 'https://xiaoyuan.space' }, // 外部链接
+      { text: 'Java学习笔记', link: '/java/' }, // 内部链接 以docs为根目录
+      // 下拉列表
       {
-   //     title: '软件资源',
-     //   children: ['/软件资源/软件资源' ],
-    //    initialOpenGroupIndex: 1 // 可选的, 默认值是 0
+        text: '已发布项目',
+        items: [
+          { text: 'SaaS短链接', link: 'http://short-link.xiaoyuan.space' }
+        ]
       }
-    ]
+    ],
+    sidebarDepth: 2,
+    search:true,
+    searchMaxSuggestions:10,
+    sidebar:
+        {
+      // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
+      '/java/': [
+          //首页 java 目录下的README.md 等于是首页
+        '/java/',
+          //自定目录
+        {
+          title: 'Java基础',
+          children: [
+            '/java/test', // 以docs为根目录来查找文件
+            // 上面地址查找的是：docs>java>test.md 文件
+            // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
+          ]
+        }
+      ],
+    }
   },
 };
